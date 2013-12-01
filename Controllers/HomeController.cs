29e01023cf4016efbd01e501c6c81e9833064289
Mark Del Castillo <peemk@hotmail.com>
2013-11-30 23:53:37 -0800

    1st Draft

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW7.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            
+            items.Add(new SelectListItem { Text = "Luxury", Value = "0", Selected = true });
+            items.Add(new SelectListItem { Text = "Standard", Value = "1" });
+            items.Add(new SelectListItem { Text = "Economic", Value = "2" });
+            ViewBag.RoomType = items;
+            
+            return View();
+        }
+
+        public ActionResult ClientSide()
+        {
+            return View();
+        }
+
+        public ActionResult ServerSide()
+        {
+            return View();
+        }
+
+        public ActionResult Absolute()
+        {
+            return View();
+        }
+
+        public ActionResult Float()
+        {
+            return View();
+        }
+
+        public ActionResult Static()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult FormatPhoneNumber(string inputNumber)
+        {
+
+            if (string.IsNullOrEmpty(inputNumber))
+            {
+                return Content("invalid input, please try again");
+            }
+            else if (inputNumber.Length != 10)
+            {
+                return Content("Please enter 10 digit number");
+            }
+            else
+            {
+                string formatted = formatNumber(inputNumber);
+                return View((object)formatted);
+            }
+        }
+
+        private string formatNumber(string inputNumber)
+        {
+            char[] cArray = inputNumber.ToCharArray();
+            string AreaCode = "(" + inputNumber.Substring(0, 3) + ")";
+            string Prefix = " " + inputNumber.Substring(3, 3) + "-";
+            string LineNumber = inputNumber.Substring(6, 4);
+
+            string formatted = AreaCode + Prefix + LineNumber;
+
+            return formatted;
+        }
+
+
+
+
+
+        
+    }
+}
---
+++ Scripts/ReverseText.js
@@ -0,0 +1,19 @@
+﻿function ReverseText() {
+    var reverse = "";
+    var s = document.getElementById("inputText").value;
+    
+    console.log("S: " + s);
+
+    var strArr = s.split(" ");
+    console.log("strArr: " + strArr);
+
+
+    for (var i = strArr.length - 1; i > -1; i--) {
+        reverse = reverse + strArr[i] + " ";
+        console.log("strArr(" + i + "), Reverse: " + reverse);
+    }
+    console.log("Reverse: " + reverse);
+    document.getElementById("result").value = reverse;
+    
+
+}
