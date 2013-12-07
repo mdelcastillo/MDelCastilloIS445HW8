67888968a992119f4775fb3600a493e733edeb53
Mark Del Castillo <peemk@hotmail.com>
2013-12-06 20:10:49 -0800

    Removed Console command lines from JS that causes crashed on IE

--- Scripts/ReverseText.js
+++ Scripts/ReverseText.js
@@ -2,17 +2,17 @@
     var reverse = "";
     var s = document.getElementById("inputText").value;
     
-    console.log("S: " + s);
+
 
     var strArr = s.split(" ");
-    console.log("strArr: " + strArr);
+
 
 
     for (var i = strArr.length - 1; i > -1; i--) {
         reverse = reverse + strArr[i] + " ";
-        console.log("strArr(" + i + "), Reverse: " + reverse);
+
     }
-    console.log("Reverse: " + reverse);
+
     document.getElementById("result").value = reverse;
     
 
